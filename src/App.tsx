c1a57531783a8821be688a62ecadd962e23173fa
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-17 21:09:51 +0900

    STYLE: グローバルスタイル作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {ChakraProvider} from "@chakra-ui/react";
+import theme from "./theme/theme";
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <ChakraProvider theme={theme}>
+      <div>Hello</div>
+    </ChakraProvider>
   );
 }
 
