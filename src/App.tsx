17c7ebb66180b771324729640d71a2c8f472a63a
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-17 21:36:07 +0900

    REFACTOR: フォーマット&型付与

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import {ChakraProvider} from "@chakra-ui/react";
-import theme from "./theme/theme";
+import React, { VFC } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme/theme'
 
-function App() {
+const App: VFC = () => {
   return (
     <ChakraProvider theme={theme}>
-      <div>Hello</div>
+      <div>Hello World</div>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
