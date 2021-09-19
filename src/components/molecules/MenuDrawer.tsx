1a313fa22be94b8a9882d10b897f1bc088bfb0ba
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-19 18:52:35 +0900

    REFACTOR: ドロワーコンポーネントで画面遷移の関数を受け取れるようにする

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -8,23 +8,38 @@ import {
   DrawerCloseButton,
 } from '@chakra-ui/react'
 
+import { RoutingFunctions } from '../organisms/layout/Header'
+
 type Props = {
   onClose: () => void
   isOpen: boolean
+  routingFunctions: RoutingFunctions
 }
 
-export const MenuDrawer: VFC<Props> = memo(({ onClose, isOpen }) => {
-  return (
-    <Drawer isOpen={isOpen} placement="left" size="xs" onClose={onClose}>
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton />
-        <DrawerBody p={0} bg="gray.100">
-          <Button w="100%">TOP</Button>
-          <Button w="100%">ユーザー一覧</Button>
-          <Button w="100%">設定</Button>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
-  )
-})
+export const MenuDrawer: VFC<Props> = memo(
+  ({
+    onClose,
+    isOpen,
+    routingFunctions: { onClickHome, onClickUserManagement, onClickSetting },
+  }) => {
+    return (
+      <Drawer isOpen={isOpen} placement="left" size="xs" onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerBody p={0} bg="gray.100">
+            <Button w="100%" onClick={onClickHome}>
+              TOP
+            </Button>
+            <Button w="100%" onClick={onClickUserManagement}>
+              ユーザー一覧
+            </Button>
+            <Button w="100%" onClick={onClickSetting}>
+              設定
+            </Button>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    )
+  }
+)
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,11 +1,29 @@
-import { memo, VFC } from 'react'
+import { memo, useCallback, VFC } from 'react'
 import { Flex, HStack, Heading, Link, useDisclosure } from '@chakra-ui/react'
 
 import { MenuIconButton } from '../../atoms/button/MenuIconButton'
 import { MenuDrawer } from '../../molecules/MenuDrawer'
+import { useHistory } from 'react-router'
+
+export type RoutingFunctions = {
+  onClickHome: () => void
+  onClickUserManagement: () => void
+  onClickSetting: () => void
+}
 
 export const Header: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const history = useHistory()
+
+  const onClickHome = useCallback(() => history.push('/home'), [])
+  const onClickUserManagement = useCallback(() => history.push('/home/user_management'), [])
+  const onClickSetting = useCallback(() => history.push('/home/setting'), [])
+
+  const routingFunctions: RoutingFunctions = {
+    onClickHome: onClickHome,
+    onClickUserManagement: onClickUserManagement,
+    onClickSetting: onClickSetting,
+  }
 
   return (
     <>
@@ -19,18 +37,18 @@ export const Header: VFC = memo(() => {
       >
         <Flex align="center" as="a" mr={8} _hover={{ cursor: 'pointer' }}>
           <Heading as="h1" fontSize={{ base: 'md', md: 'lg' }}>
-            ユーザー管理アプリ
+            <Link onClick={onClickHome}>ユーザー管理アプリ</Link>
           </Heading>
         </Flex>
         <Flex align="center" fontSize="sm" flexGrow={2} display={{ base: 'none', md: 'flex' }}>
           <HStack spacing={4}>
-            <Link>ユーザー一覧</Link>
-            <Link>設定</Link>
+            <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
+            <Link onClick={onClickSetting}>設定</Link>
           </HStack>
         </Flex>
         <MenuIconButton onOpen={onOpen} />
       </Flex>
-      <MenuDrawer isOpen={isOpen} onClose={onClose} />
+      <MenuDrawer isOpen={isOpen} onClose={onClose} routingFunctions={routingFunctions} />
     </>
   )
 })
