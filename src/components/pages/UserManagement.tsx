b167d8aea65fabb0a10eb52f9cf5585d5c2d4365
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-20 14:49:26 +0900

    ADD: ユーザー一覧画面作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
 import { memo, VFC } from 'react'
+import { Wrap, WrapItem, Box, Stack, Image, Text } from '@chakra-ui/react'
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページ</p>
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: 'pointer', opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              src="https://source.unsplash.com/random"
+              boxSize="160px"
+              borderRadius="full"
+              m="auto"
+              alt="プロフィール画像"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              ほげ
+            </Text>
+            <Text fontSize="sm" color="gray">
+              hoge
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  )
 })
