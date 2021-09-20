da3ca7f16a667656ab3d23b0e6483ac64aae5525
Taiji-Kondo <k.taiji1108@gmail.com>
2021-09-20 14:13:32 +0900

    ADD: PrimaryButtonにonClickイベントを受け取れるようにする

--- src/components/atoms/button/PrimaryButton.tsx
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -2,12 +2,13 @@ import { memo, ReactNode, VFC } from 'react'
 import { Button } from '@chakra-ui/react'
 
 type Props = {
+  onClick: () => void
   children: ReactNode
 }
 
-export const PrimaryButton: VFC<Props> = memo(({ children }) => {
+export const PrimaryButton: VFC<Props> = memo(({ children, onClick }) => {
   return (
-    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }} onClick={onClick}>
       {children}
     </Button>
   )
